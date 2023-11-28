aacaa8bac66680ba8d46622e3fe5bf82bb766d0e
LeperIV <152258583+LeperIV@users.noreply.github.com>
2023-11-28 10:35:31 +0300

    Add files via upload

---
+++ pages/__init__.py
@@ -0,0 +1,3 @@
+from pages.pages_info import pages_dict
+from pages.pages_info import pages_names
+from pages.pages import (PageContainer, WelcomePage, SportPage, GuidePage, UserPage)
---
+++ pages/pages_info.py
@@ -0,0 +1,10 @@
+pages_names = {
+    'спорт': 'sport',
+    'руководство': 'guide',
+    'профиль': 'user'
+}
+pages_dict = {
+    'sport': None,
+    'guide': None,
+    'user': None
+}
